54512c62ab1871fdcb9191e9524036df2cb712a2
Aaron Abbott <aabmass@gmail.com>
2017-04-28 16:37:56 -0400

    Use base64 (url) encoding instead of percent encoding

--- main.js
+++ main.js
@@ -5,6 +5,33 @@
 var latexInput = document.getElementById('latex-input');
 var latexOutput = document.getElementById('latex-output');
 
+function base64UrlEncode(text) {
+  var nonUrlSafe = btoa(text);
+
+  // now replace replace the non-safe chars:
+  //
+  // + -> -
+  // / -> _
+  //
+  // and the padding ('='):
+  // = -> ''
+  return nonUrlSafe.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+function base64UrlDecode(encodedText) {
+  // do the opposite of base64UrlEncode()
+  var nonUrlSafe = encodedText.replace('-', '+').replace('_', '-');
+
+  // now fix the padding
+  var extraSize = nonUrlSafe.length % 4;
+  if (extraSize === 2)
+    nonUrlSafe += '==';
+  else if (extraSize === 3)
+    nonUrlSafe += '=';
+
+  return atob(nonUrlSafe);
+}
+
 // finds all items of class katex-name and renders them with logo
 function renderKatexClass() {
   var els = document.getElementsByClassName('katex-name');
@@ -20,19 +47,26 @@ function initialParseQueryString() {
 
   if (parts.length < 2) return;
 
-  latexInput.value = decodeURI(parts.slice(1).join('#'));
+  // catch any decoding errors
+  try {
+    latexInput.value = base64UrlDecode(parts.slice(1).join('#'));
+  }
+  catch(e) {
+    // the encoded url wasn't valid, just trim it and continue on..
+    window.location.href = parts[0] + '#';
+    latexInput.value = '';
+  }
 }
 
 function updateQueryString(latexInput) {
   // try and split it by '?'
   var parts = window.location.href.split('#');
 
-  window.location.href = parts[0] + '#' + encodeURI(latexInput);
+  window.location.href = parts[0] + '#' + base64UrlEncode(latexInput);
 }
 
 function renderKatex(latexString) {
   katex.render(latexString, latexOutput, { displayMode: true });
-  console.log('rerendering');
 }
 
 function attachListeners() {
